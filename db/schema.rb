8a073c215d06acf517ee3f3052afac99c8151736
Ben Yan Hao Tai <bentyh@hotmail.com>
2019-03-28 22:45:21 +0000

    HW3 update

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_021904) do
+ActiveRecord::Schema.define(version: 2019_03_28_214503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "author", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.string "genre"
     t.string "classification"
     t.string "book_type"
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sub_title"
   end
 
 end
